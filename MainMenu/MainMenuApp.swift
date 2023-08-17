f2b0912945fc4a443376c36fe11d095a52210550
Jay Ram <jr2023pgm@gmail.com>
2023-08-17 20:37:57 +0400

    Initial Commit

---
+++ MainMenu/MainMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuApp.swift
+//  MainMenu
+//
+//  Created by jr on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MainMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
